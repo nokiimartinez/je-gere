62e457d24213e1cf4ab6330e2297bf6dee058298
nokiimartinez <nicolasjean.martinez@gmail.com>
2021-11-05 12:13:20 +0100

    petit test react

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './header.js';
+import Navbar from './navbar.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <Navbar/>
   </React.StrictMode>,
   document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/navbar.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Navbar() {
+    return ( 
+    <div className="navbar"> 
+        Test 
+    </div> 
+    );
+}
+
+export default Navbar ;
